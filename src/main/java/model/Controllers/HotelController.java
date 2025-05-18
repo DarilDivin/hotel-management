932f8a27dc24352d7c77b114f361c96ad3a1a6b4
josé-Marie <laourouangelin00@gmail.com>
2025-05-18 10:00:51 +0200

    feat: création du HotelController

--- src/main/java/model/Controllers/HotelController.java
+++ src/main/java/model/Controllers/HotelController.java
@@ -1,4 +1,31 @@
 package model.Controllers;
 
+import model.Hotel;
+import model.DAO.HotelDAO;
+
+import java.util.Vector;
+
 public class HotelController {
+    private HotelDAO hotelDAO = new HotelDAO();
+
+    public void AjouterHotel(String nom, String adresse) {
+        Hotel hotel = new Hotel(nom, adresse);
+        hotelDAO.ajouterHotel(hotel);
+    }
+
+    public Vector<Hotel> afficherHotels() {
+        return hotelDAO.getAllHotels();
+    }
+
+    public Hotel getHotel(int id) {
+        return hotelDAO.getHotelById(id);
+    }
+
+    public void modifierHotel(Hotel hotel) {
+        hotelDAO.modifierHotel(hotel);
+    }
+
+    public void supprimerHotel(Hotel hotel) {
+
+    }
 }
